342e3ebbe5e0ffca652a59bfb02c346e33bd8df5
eitanLieberman <eitanl95@gmail.com>
2022-01-22 17:43:29 +0200

    chat api done

--- routes/chat.js
+++ routes/chat.js
@@ -8,6 +8,7 @@ const {
 
 const router = require("express").Router();
 
+//start/access a chat
 router.post("/", verifyToken, async (req, res) => {
   const { userId } = req.body;
   if (!userId) {
@@ -48,6 +49,7 @@ router.post("/", verifyToken, async (req, res) => {
   }
 });
 
+//display all user chats
 router.get("/", verifyToken, async (req, res) => {
   try {
     const findChat = await Chat.find({
@@ -68,3 +70,115 @@ router.get("/", verifyToken, async (req, res) => {
 });
 
 module.exports = router;
+
+//create a group chat
+router.post("/group", verifyToken, async (req, res) => {
+  if (!req.body.users || !req.body.name) {
+    return res.status(400).send({ message: "Please Fill all the feilds" });
+  }
+  var users = JSON.parse(req.body.users);
+
+  if (users.length < 2) {
+    return res
+      .status(400)
+      .send("More than 2 users are required to form a group chat");
+  }
+  users.push(req.user.id);
+
+  try {
+    const groupChat = await Chat.create({
+      chatName: req.body.name,
+      users: users,
+      isGroupChat: true,
+      groupAdmin: req.user.id,
+    });
+
+    console.log(groupChat);
+    const fullGroupChat = await Chat.findOne({ _id: groupChat._id })
+      .populate("users", "-password")
+      .populate("groupAdmin", "-password");
+    res.status(200).json(fullGroupChat);
+  } catch (err) {
+    res.status(400).json(err);
+  }
+});
+
+//change chat name
+
+router.put("/rename", verifyToken, async (req, res) => {
+  try {
+    const updatedChat = await Chat.findByIdAndUpdate(
+      req.body.chatId,
+
+      {
+        chatName: req.body.chatName,
+      },
+      {
+        new: true,
+      }
+    )
+      .populate("users", "-password")
+      .populate("groupAdmin", "-password");
+
+    res.status(201).json(updatedChat);
+  } catch (err) {
+    res.status(400).json(err);
+  }
+});
+
+//remove from chat
+
+router.put("/remove", verifyToken, async (req, res) => {
+  const { chatId, userId } = req.body;
+  console.log(req.user.id);
+  const targetChat = await Chat.findById(chatId);
+
+  if (req.user.id !== targetChat.groupAdmin.toString()) {
+    res.status(404);
+    throw new Error("not group admin");
+  }
+
+  const removed = await Chat.findByIdAndUpdate(
+    chatId,
+    {
+      $pull: { users: userId },
+    },
+    {
+      new: true,
+    }
+  )
+    .populate("users", "-password")
+    .populate("groupAdmin", "-password");
+
+  res.status(201).json(removed);
+});
+
+router.put("/invite", verifyToken, async (req, res) => {
+  const { chatId, userId } = req.body;
+  console.log(req.user.id);
+  const targetChat = await Chat.findById(chatId);
+  //can't add a user who is already in group
+  targetChat.users.forEach((user) => {
+    if (user.toString() === userId) {
+      res.status(400).json("already in group");
+    }
+  });
+
+  if (req.user.id !== targetChat.groupAdmin.toString()) {
+    res.status(404).json("not group admin");
+  }
+
+  const removed = await Chat.findByIdAndUpdate(
+    chatId,
+    {
+      $push: { users: userId },
+    },
+    {
+      new: true,
+    }
+  )
+    .populate("users", "-password")
+    .populate("groupAdmin", "-password");
+
+  res.status(201).json(removed);
+});
